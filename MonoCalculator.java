c9a42acd620266014c5404b9f48726181bcd260c
LoneDespairYt <lonedespairyt@gmail.com>
2022-11-27 23:45:30 +0800

    sync changes

---
+++ MonoCalculator.java
@@ -0,0 +1,12 @@
+package com.mycompany.monocalculator;
+
+
+public class MonoCalculator {
+
+    public static void main(String[] args) {
+        new Classic().setVisible(true);
+        new Classic().setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+}
